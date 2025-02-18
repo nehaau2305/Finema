611ba4b696cd3bae4cf8d6c5cd32f9457454f1a9
nehaau2305 <nehaau2305@gmail.com>
2025-02-18 02:16:51 -0500

    updated MovieController.java

--- backend/src/main/java/com/TermProject/finema/controller/MovieController.java
+++ backend/src/main/java/com/TermProject/finema/controller/MovieController.java
@@ -4,14 +4,22 @@ import com.TermProject.finema.entity.Movie;
 import com.TermProject.finema.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 @RestController
-//@RequestMapping("")
+@RequestMapping
 public class MovieController {
     @Autowired
     private MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
+        Movie savedMovie = movieService.addMovie(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
+    }
 
     //ADD METHODS THAT HANDLES HTTP REQUESTS
     //SO NEED @GetMapping OR @PostMapping
