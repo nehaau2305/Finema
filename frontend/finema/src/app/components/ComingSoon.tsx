1e98700636cf351a4b2cd8f042cd6e55e9e266fd
nehaau2305 <nehaau2305@gmail.com>
2025-02-26 23:29:10 -0500

    coming soon and now playing frontend

--- frontend/finema/src/app/components/ComingSoon.tsx
+++ frontend/finema/src/app/components/ComingSoon.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+
 import MovieCard from '../components/MovieCard'
 import styles from './ComingSoon.module.css'
+import React, { useState, useEffect } from 'react';
+
+interface MovieCardProps {
+  name: string;
+  source: string;
+  movieId: number; 
+}
+
 
 export default function ComingSoon() {
+  const [comingSoonMovies, setComingSoonMovies] = useState<MovieCardProps[]>([]);
+
+  // fetch coming soon movies from backend
+  useEffect(() => {
+    const fetchComingSoonMovies = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/movies/coming-soon');
+        const data = await response.json();
+        setComingSoonMovies(data);
+      } catch (error) {
+        console.error("Error fetching coming soon movies:", error);
+      }
+    };
+
+    fetchComingSoonMovies();
+  }, []);
+
+  // display the coming soon movies
   return (
     <div className={styles.main_body}>
         <h1 className={styles.header}>coming soon</h1>
         <section>
           <ul className={styles.list}>
-            <li><MovieCard name='Time Bandits' source='timebandits.png' /></li>
+            {/*<li><MovieCard name='Time Bandits' source='timebandits.png' /></li>*/}
+            {comingSoonMovies.length > 0 ? (
+            comingSoonMovies.map((movie) => (
+              <li key={movie.movieId}>
+                <MovieCard name={movie.name} source={movie.source} movieId={movie.movieId} />
+              </li>
+            ))
+          ) : (
+            <p>No coming soon movies available at the moment.</p>
+          )}
           </ul>
         </section>
     </div>
--- frontend/finema/src/app/components/NowPlaying.tsx
+++ frontend/finema/src/app/components/NowPlaying.tsx
@@ -1,9 +1,15 @@
 'use client'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import { useRouter } from 'next/navigation'
 //import Button from '../components/Button'
 import MovieCard from './MovieCard';
 import styles from './NowPlaying.module.css'
+
+interface MovieCardProps {
+  name: string;
+  source: string;
+  movieId: number; 
+}
 
 export default function NowPlaying() {
   //const router = useRouter()
@@ -14,13 +20,41 @@ export default function NowPlaying() {
   const handleLogIn = () => {
     router.push('/loggedin-user-home')
   }*/
+
+  const [nowShowingMovies, setNowShowingMovies] = useState<MovieCardProps[]>([]);
+  
+    // fetch coming soon movies from backend
+    useEffect(() => {
+      const fetchNowShowingMovies = async () => {
+        try {
+          const response = await fetch('http://localhost:8080/movies/now-showing');
+          const data = await response.json();
+          setNowShowingMovies(data);
+        } catch (error) {
+          console.error("Error fetching now showing movies:", error);
+        }
+      };
+  
+      fetchNowShowingMovies();
+    }, []);
+  
+
   return (
     <div className={styles.main_body}>
         <h1 className={styles.header}>now playing</h1>
         <section>
           <ul className={styles.list}>
-            <li><MovieCard name='Time Bandits' source='timebandits.png' /></li>
-            <li><MovieCard name='Les Miserables (1952)' source='les-mis.png' /></li>
+            {/*<li><MovieCard name='Time Bandits' source='timebandits.png' /></li>*/}
+            {/*<li><MovieCard name='Les Miserables (1952)' source='les-mis.png' /></li>*/}
+            {nowShowingMovies.length > 0 ? (
+              nowShowingMovies.map((movie) => (
+                <li key={movie.movieId}>
+                  <MovieCard name={movie.name} source={movie.source} movieId={movie.movieId} />
+                </li>
+               ))
+              ) : (
+                <p>No coming soon movies available at the moment.</p>
+            )}
           </ul>
         </section>
     </div>
