ec33cb7bc0cf482f683b02542c88842551ac25f3
nehaau2305 <nehaau2305@gmail.com>
2025-03-17 16:29:02 -0400

    fixed 403 Forbidden error for register

--- backend/src/main/java/com/TermProject/finema/controller/AuthController.java
+++ backend/src/main/java/com/TermProject/finema/controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
     private AuthService authService;
 
 
