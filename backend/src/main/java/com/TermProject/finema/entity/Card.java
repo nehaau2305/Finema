5890bebf834953de461814b06a58ed8fdf09c7dc
MaxAllgaier <mja25912@uga.edu>
2025-03-21 16:35:46 -0400

    card updated

--- backend/src/main/java/com/TermProject/finema/entity/Card.java
+++ backend/src/main/java/com/TermProject/finema/entity/Card.java
@@ -1,22 +1,35 @@
 package com.TermProject.finema.entity;
 
 import jakarta.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "card")
 public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer cardID;
 
+    private Integer userID;
+
+    @Column(name = "cardNumber", unique = true, nullable = false, length = 16)
     private String cardNumber;
+
+    @Column(name = "cardholderName", nullable = false, length = 255)
     private String cardholderName;
+
+    @Column(name = "expirationDate", nullable = false, length = 5)
     private String expirationDate;
-    private String billingAddress;
+
+    @Column(name = "cvv", length = 255)
     private String cvv;
 
+
+    @Column(name = "billing_address", length = 255)
+    private String billingAddress;
+
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "userID", nullable = false)
     private User user;
 
     public Card() {
@@ -24,20 +37,21 @@ public class Card {
     }
 
     public Card(String cardNumber, String cardholderName, String expirationDate, String billingAddress, String cvv, User user) {
+        this.cardID = 0;
         this.cardNumber = cardNumber;
         this.cardholderName = cardholderName;
         this.expirationDate = expirationDate;
         this.billingAddress = billingAddress;
         this.cvv = cvv;
-        this.user = user;
+        this.userID = user.getId();
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getCardID() {
+        return cardID;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setCardID(Integer cardID) {
+        this.cardID = cardID;
     }
 
     public String getCardNumber() {
@@ -48,7 +62,6 @@ public class Card {
         this.cardNumber = cardNumber;
     }
 
-    
     public String getCardholderName() {
         return cardholderName;
     }
@@ -57,13 +70,20 @@ public class Card {
         this.cardholderName = cardholderName;
     }
 
-
     public String getExpirationDate() {
         return expirationDate;
     }
 
     public void setExpirationDate(String expirationDate) {
         this.expirationDate = expirationDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
     }
 
     public String getBillingAddress() {
@@ -74,15 +94,6 @@ public class Card {
         this.billingAddress = billingAddress;
     }
 
-    
-    public String getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(String cvv) {
-        this.cvv = cvv;
-    }
-
     public User getUser() {
         return user;
     }
@@ -90,4 +101,5 @@ public class Card {
     public void setUser(User user) {
         this.user = user;
     }
+
 }
