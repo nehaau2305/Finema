825b555f249faf1131e046b3bebf5f2f7fdc145a
MaxAllgaier <mja25912@uga.edu>
2025-04-30 07:32:37 -0400

    reviews shown on movie popup

--- frontend/finema/src/app/components/MovieCard.tsx
+++ frontend/finema/src/app/components/MovieCard.tsx
@@ -29,10 +29,10 @@ interface MovieCardProps {
 
 export default function MovieCard({ name, source, movieId, synopsis, director, producer, mpaaRating, cast}: MovieCardProps) {
   const [isOpened, setIsOpened] = useState(false);
-  const [reviews, setReviews] = useState<Review[]>([])
+  //const [reviews, setReviews] = useState<Review[]>([])
   const [showTimes, setShowTimes] = useState<ShowTime[]>([]);
 
-  useEffect(() => {
+  /*useEffect(() => {
     fetch('http://localhost:8080/review/movie/' + movieId, {
       method: 'GET',
       headers: {
@@ -43,7 +43,7 @@ export default function MovieCard({ name, source, movieId, synopsis, director, p
       .then(response => response.json())
       .then(data => setReviews(data))
       .catch(error => console.error('Error fetching reviews:', error));
-  }, [])
+  }, [])*/
 
   useEffect(() => {
     // Fetch showtimes for the movie
@@ -96,7 +96,6 @@ export default function MovieCard({ name, source, movieId, synopsis, director, p
         director={director}
         producer={producer}
         cast={cast}
-        reviews={reviews}
       />
 
     </div>
